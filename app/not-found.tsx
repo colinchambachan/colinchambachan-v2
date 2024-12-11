5c8c166be8a24ae117e897f57f1a4f09b1984d32
Colin Chambachan <colin.chambachan@gmail.com>
2024-12-11 12:45:21 -0500

    feat: create custom 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function NotFoundPage() {
-  return <h1>Page not found</h1>;
+  return (
+    <div className="place-self-center text-center">
+      <div className="mb-3">Wrong way!</div>
+      <Link href={"/"} className="btn btn-outline">
+        Main Page
+      </Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
