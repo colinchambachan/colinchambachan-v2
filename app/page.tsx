ff845dc99a6979603c7e32346ed4a1a60fffe7a0
Colin Chambachan <colin.chambachan@gmail.com>
2024-12-11 16:27:47 -0500

    feat: create hero cheatsheet

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,207 @@
 "use client";
 import Image from "next/image";
 import hero from "/public/hero.jpg";
+import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
+  type Paragraph = string[];
+  const [isQuickCopied, setQuickCopied] = useState<Boolean>(false);
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const [quickContent, setQuickContent] = useState<string[][]>([]);
+  const [firstTwoLines, setFirstTwoLines] = useState<string[]>([]);
+
+  const handleModalClose = () => {
+    setQuickCopied(false);
+  };
+
+  const handleClipboardCopy = () => {
+    let content = "";
+
+    firstTwoLines.forEach((line) => {
+      content += line + "\n";
+    });
+
+    quickContent.forEach((section, index) => {
+      section.forEach((line) => {
+        content += line + "\n";
+      });
+      content += "\n";
+    });
+
+    // Copy the formatted content to clipboard
+    navigator.clipboard
+      .writeText(content)
+      .then(() => {
+        setQuickCopied(true);
+        setTimeout(() => setQuickCopied(false), 2000); // Reset after 2 seconds
+      })
+      .catch((error) => {
+        console.error("Failed to copy text:", error);
+      });
+  };
+
+  useEffect(() => {
+    // Fetch the text file
+    const fetchTextFile = async () => {
+      try {
+        const response = await fetch("/data/quick.txt");
+        if (!response.ok) {
+          throw new Error(`Failed to fetch file: ${response.statusText}`);
+        }
+        const text = await response.text();
+        const lines = text.split("\n");
+        setFirstTwoLines(lines.slice(0, 2));
+
+        const linesToRender = lines.slice(2);
+
+        const paragraphs: Paragraph[] = linesToRender.reduce<Paragraph[]>(
+          (acc, line) => {
+            if (line.trim() === "") {
+              acc.push([]);
+            } else {
+              if (acc.length === 0) acc.push([]);
+              acc[acc.length - 1].push(line);
+            }
+            return acc;
+          },
+          []
+        );
+
+        setQuickContent(paragraphs);
+      } catch (error) {
+        console.error("Error fetching the file:", error);
+      }
+    };
+
+    fetchTextFile();
+  }, []);
+
   return (
     <div className="flex-col justify-items-center content-between">
-      <div className="hero mt-[-10rem] min-h-screen">
+      <div className="hero mt-[-5rem] mb-[-5rem] md:mb-0 md:mt-[-10rem] min-h-screen">
         {/* <div className="hero h-96"> */}
         <div className="flex justify-center text-center xl:justify-normal md:text-left">
           <Image
             src={hero}
             className="max-w-xs 2xl:max-w-sm md:mx-8 xl:mx-12 hidden md:block rounded-full shadow-2xl"
             alt="hi"
+            priority={true}
           />
           <div className="mt-auto mb-auto">
             <h1 className="text-5xl font-bold">Hi, I'm Colin</h1>
-            <p className="py-6">Passionate about Cloud, Data, and AI</p>
+            <h2 className="py-2 text-gray-500">
+              <em>Currently: Data + AI @ RBC</em>
+            </h2>
+            <p className="pt-1 pb-4 text-primary font-extrabold">
+              Passionate about Cloud, Data, and AI
+            </p>
             <button
               className="btn btn-primary"
-              onClick={() => document.getElementById("my_modal_2")?.showModal()}
+              onClick={() =>
+                document.getElementById("quick_modal")?.showModal()
+              }
             >
               View Cheatsheet
             </button>
+            <nav className="flex md:hidden py-4 place-self-center">
+              <a
+                href={"https://www.linkedin.com/in/colinchambachan/"}
+                target="_blank"
+              >
+                {/* linkedin */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  className="fill-current"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                </svg>
+              </a>
+              <a href={"https://github.com/colinchambachan"} target="_blank">
+                {/* github */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  className="fill-current"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+              </a>
+              <a href={"mailto:colin.chambachan@gmail.com"} target="_blank">
+                {/* mail */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  className="fill-current"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                </svg>
+              </a>
+            </nav>
           </div>
         </div>
       </div>
-      <dialog id="my_modal_2" className="modal">
+      {/* Modal */}
+      <dialog
+        id="quick_modal"
+        className="modal"
+        ref={modalRef}
+        onClose={handleModalClose}
+      >
         <div className="modal-box">
           <h3 className="font-bold text-lg">~/Colin.zip</h3>
           {/* <h6 className="text-gray-500">$ unzip Colin.zip</h6> */}
-          <h6 className="text-gray-500">$ cat Colin/quick.txt</h6>
-          <p className="py-4">Press ESC key or click outside to close</p>
+          <h6 className="text-gray-500">
+            $ unzip Colin.zip && cat Colin/quick.txt
+          </h6>
+          {/* <p className="py-4">Press ESC key or click outside to close</p> */}
+          <div className="bg-gray-100 p-4 mt-3 rounded-md">
+            <div className="mb-3 flex justify-between">
+              <strong>Colin Chambachan</strong>
+              <div
+                // TODO: add save to clipboard logic
+                onClick={() => {
+                  handleClipboardCopy();
+                  setQuickCopied(true);
+                }}
+                className="transition-colors duration-600 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded-md mt-[-0.3rem] px-2 py-1"
+              >
+                {!isQuickCopied && (
+                  <i className="bi bi-clipboard text-gray-500"></i>
+                )}
+                {isQuickCopied && (
+                  <i className="bi bi-clipboard-check text-gray-500"></i>
+                )}
+              </div>
+            </div>
+            <div>
+              {quickContent.map((paragraph: Paragraph, index: number) => (
+                <div key={index} style={{ marginBottom: "1rem" }}>
+                  {paragraph.map((line: string, lineIndex: number) => (
+                    <p
+                      key={lineIndex}
+                      className={`${
+                        lineIndex === 0
+                          ? "font-bold underline"
+                          : "text-gray-600"
+                      }`}
+                      style={{
+                        margin: 0,
+                      }}
+                    >
+                      {line}
+                    </p>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
